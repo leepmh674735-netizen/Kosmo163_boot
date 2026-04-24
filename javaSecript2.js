9f78f516b06a5d5754e2c664511a9c75261afa60
Yubin <leepmh674735@gmail.com>
2026-04-24 23:07:19 +0900

    JaveScript
    one

---
+++ javaSecript2.js
@@ -0,0 +1,19 @@
+<script>
+    function newRegister() {
+   const newIten = document.createElement("li");
+    const subject = document.querySelector("subject");
+    const newText = document.createTextNode(subject.value);
+
+    newItem.appendChid(newText);
+
+    const itemList = document.querySelector("#itemList");
+    itemList.appendChid(newItem);
+
+
+    subject.value="";
+ }
+</script>
+document.addEventListener("DOMContentLoaded", function () {
+
+});
+</script >
