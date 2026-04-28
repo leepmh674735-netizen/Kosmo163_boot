716931c26a84ad698ed8434e6816aa43f17155e8
Yubin <leepmh674735@gmail.com>
2026-04-28 23:59:48 +0900

    28

---
+++ CartDTO.java
@@ -0,0 +1,15 @@
+package com.winter.app.cart;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CartDTO {
+
+	private String username;
+	private Long productNum;
+
+}
